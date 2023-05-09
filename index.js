64f022d3d06fcf2a11b4906bdcdb9b31af997cec
Thomas <tmrtw8@gmail.com>
2023-05-08 19:53:41 -0600

    skeleton

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const generateMarkdown = require("./utils/generateMarkdown");
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description of your project:",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the installation instructions for your project?",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is the usage information for other users for this project?",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select the license you used for this project.",
+        choices: [
+            "Apache",
+            "Boost",
+            "GNU AGPLv3",
+            "GNU GPLv3",
+            "GNU LGPLv3",
+            "MIT",
+            "Mozilla",
+        ],
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Can people contribute to your project, if so how?",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What are the testing protocols you used for your project?",
+    },
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "userEmail",
+        message: "What is your email?",
+    },
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+        const answers = await inquirer.prompt(questions);
+        answers.licenseBadge = licenseBadge(answers.license);
+        let readMeData = generateMarkdown(answers);
+        writeToFile();
+    } catch (err) {
+        throw err;
+    }
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,10 +10,80 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
+
+function licenseBadge(value) {
+  if (value === "Apache") {
+    return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (value === "Boost") {
+    return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+  } else if (value === "GNU AGPLv3") {
+    return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+  } else if (value === "GNU GPLv3") {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (value === "GNU LGPLv3") {
+    return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+  } else if (value === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (value === "Mozilla") {
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+  } else {
+    return "";
+  }
+}
+
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+    ${data.description}
+    ${data.licenseBadge}
+      
+    ---
+    ## Contents
 
+    1. [Installation](#installation)
+    2. [Usage](#usage)
+    3. [License](#license)
+    4. [Contributing](#contributing)
+    5. [Tests](#tests)
+    6. [Contact](#contact)
+
+
+    ---
+
+    ## Installation:
+    ${data.installation}
+
+
+    ---
+
+    ## License
+    ${data.usage}
+    
+
+    ---
+
+    ## License
+    ${data.license}
+
+
+    ---
+
+    ## Contributing:
+    ${data.contributing}
+
+
+    ---
+
+    ## Tests:
+    ${data.test}
+    
+
+    ---
+
+    ## Contact Information:
+    * GitHub Username: ${data.userName}
+    * Email: ${data.userEmail}  
 `;
 }
 
